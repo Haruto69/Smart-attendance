999632ce17777015f26b5fcbfda260744edf0882
Mukund V <100371147+Haruto69@users.noreply.github.com>
2025-06-01 21:51:13 +0530

    updated urls

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from django.contrib import admin
 from . import views
 
 app_name = 'attendance'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('attendance/', views.attendance_view, name='attendance_page'),
+    path('logout/', views.logout_view, name='logout'),
 ]
